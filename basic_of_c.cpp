114f0b71a1bff03851ba81fe93f159f60ca2e728
Mitesh Viras <virashmitesh@gmail.com>
2024-01-25 23:26:54 +0530

    wip

--- basic_of_c.cpp
+++ basic_of_c.cpp
@@ -1,3 +1,7 @@
+/*
+clrscr() is an abbreviation of the clear screen. It aims to clear the console screen. clrscr() is a library function located in the console input output header file <conio.h>. The previous screen on the console is cleared whenever the clrscr () is invoked in a program. To call the clrscr() function, it must define inside any function or the main function.
+*/
+
 #include <stdio.h>
 /*
 iostream stands for standard input-output stream. #include iostream declares objects that control reading from and writing to the standard streams. In other words, the iostream library is an object-oriented library that provides input and output functionality using streams.
@@ -13,13 +17,25 @@ using namespace std;
 int main()
 {
     int a, b, sum;
-    cout << "hello \n please enter values.\n";
+    string username = "User";
+
+    cout
+        << "Enter your name : ";
+    cin >> username;
+    cout << "Dear " << username << " this program is for testing purpose." << endl;
+
+    cout << "now we are going to do sum of two values." << endl;
+
+    cout
+        << "please enter values." << endl;
     cout << "enter value for a : ";
     cin >> a;
     cout << "enter value for b : ";
     cin >> b;
     sum = a + b;
-    cout << "\n\nthe sum of " << a << " and " << b << " is " << sum << "\n\n";
+    cout << "\n\nthe sum of " << a << " and " << b << " is " << sum << endl
+         << endl;
+    cout << "bye bye " << username << ".";
 
     return 0;
 }
