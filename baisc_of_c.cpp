54b9a52dd481d7a4f823620fb2cd3a7d450ed55c
Mitesh Viras <virashmitesh@gmail.com>
2024-01-25 22:50:41 +0530

    init

---
+++ baisc_of_c.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+iostream stands for standard input-output stream. #include iostream declares objects that control reading from and writing to the standard streams. In other words, the iostream library is an object-oriented library that provides input and output functionality using streams.
+*/
+#include <iostream>
+
+/*
+The std is a short form of standard, the std namespace contains the built-in classes and declared functions. You can find all the standard types and functions in the C++ "std" namespace. There are also several namespaces inside "std."
+*/
+using namespace std;
+
+// main() is where program execution begins.
+int main()
+{
+    cout << "hello";
+    return 0;
+}
